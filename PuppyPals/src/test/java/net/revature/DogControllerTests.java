ef70d1f310fbac0eeeda50e93f7520a871dcae6f
unknown <cab886@gmail.com>
2022-02-03 13:39:48 -0800

    tests that still need tinkering

---
+++ PuppyPals/src/test/java/net/revature/DogControllerTests.java
@@ -0,0 +1,20 @@
+package net.revature;
+
+//@WebMvcTest(DogController.class)
+public class DogControllerTests {
+	
+//	@MockBean
+//	private AccountService AccountService;
+//
+//	@Autowired
+//	private MockMvc mockMvc;
+//	
+//	@Test
+//	void saveFailsWhenNoDog() throws Exception {
+//		mockMvc.perform((RequestBuilder) ((ResultActions) post("/account/new")
+//				.contentType(MediaType.APPLICATION_JSON)
+//				.content("{}"))
+//				.andExpect(status().isBadRequest()));
+//	}
+
+}
